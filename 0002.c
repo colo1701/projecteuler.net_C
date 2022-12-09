a9ca03dd1347fb115f83854b9ef0b73b5250a87f
colo1701 <38246533+colo1701@users.noreply.github.com>
2022-12-09 22:53:15 +0100

    Update 0002.c

--- 0002.c
+++ 0002.c
@@ -1,10 +1,20 @@
+/*******************************************************************************
+                                 Exercise 0002:
+                                 
+Write a C program to print a block F using hash (#), where the F has a height of
+             five characters and width of five and four characters.
+                          And also to print a big 'C'.
+             
+*******************************************************************************/
+
 #include <stdio.h>
-// Write a C program to print a block 'F' using hash (#), where the F has a height of five characters and width of five and four characters. And also to print a big 'C'.
+
 int main() {
     printf("#####  ### \n");
     printf("#     #   #\n");
     printf("####  #    \n");
     printf("#     #   #\n");
     printf("#      ### \n");
+    
     return 0;
 }
