68d1ef740245c01d8c38aa5d9396f0fb5c68849d
colo1701 <38246533+colo1701@users.noreply.github.com>
2023-01-05 17:01:48 +0100

    Update 001.c

--- 001.c
+++ 001.c
@@ -4,14 +4,12 @@ int main()
 {
     int a = 0;
     int res = 0;
-    
     while(a < 999) {
         a++;
         if(a%5 == 0 || a%3 == 0) {
             res = res + a;
         }
     }
-  
-    printf("The sum of all multiples of 3 & 5 below 1000 is %d.\n", res);
+    printf("The result is %d.\n", res);
     return 0;
 }
