2a8a4addbd1699b1320224e32ab2cd3573133669
colo1701 <38246533+colo1701@users.noreply.github.com>
2023-01-05 16:49:11 +0100

    Update 0004_even_fibonacci_sol.c

--- 0004_even_fibonacci_sol.c
+++ 0004_even_fibonacci_sol.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
-int main() {
-    printf("tbd\n");
+int main()
+{
+    int res = 2;                // The Sum
+    int low = 1;                // The Lower Value
+    int high = 2;               // The Higher Value
+    int next = 0;               // The Next Fibonacci Number
+    while(high < 4000000) {
+        next = low + high;      // Build Next Number
+        if(next%2 == 0) {       // Check If It Is Even
+            res = res + next;   // If Yes Add To Sum
+        }
+        low = high;
+        high = next;
+    }
+    printf("The result is %d.\n", res);
     return 0;
 }
