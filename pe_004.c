0b79af36d0c2bcc7dc9695086ca30ce1dce36c0b
colo1701 <38246533+colo1701@users.noreply.github.com>
2023-01-06 09:56:04 +0100

    Update pe_004.c

--- pe_004.c
+++ pe_004.c
@@ -6,5 +6,12 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 #include <stdio.h>
 
 int main() {
+    int a;
+    int b;
+
+    // 2 for loops for a and b for the range (100, 1000)
+    // build product and check
+    //check if some elements of loop can be skipped because of redundance
+    
     return 0;
 }
